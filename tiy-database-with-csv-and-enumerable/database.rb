cb718f3e844fa97022b50f4bf13d30d44930f0e8
Jose A. Campos <jjoseccampos@gmail.com>
2017-03-25 15:45:39 -0400

    working on csv

--- tiy-database-with-csv-and-enumerable/database.rb
+++ tiy-database-with-csv-and-enumerable/database.rb
@@ -14,39 +14,33 @@ class Person
   end
 end
 
-
-# CSV.open("employees.csv", "w") do |csv|
-#   csv << [name, phone_number, address, position, salary, slack_account, github_account]
-#   accounts.each do ||
-#     csv << [acc[:name]]
-#   end
-# end
-
 class Database
   def initialize
     @people = []
   end
 
   def add_peep
-      puts "What is the new person's name? "
-      name = gets.chomp
-      puts "What is the person's phone number? "
-      phone_number = gets.chomp
-      puts "What is the person's address? "
-      address = gets.chomp
-      puts "What is the person's position or title? "
-      position = gets.chomp
-      puts "What is the person's salary? "
-      salary = gets.chomp
-      puts "What is the person's slack handle? "
-      slack_account = gets.chomp
-      puts "What is the person's GitHub user? "
-      github_account = gets.chomp
+    puts "What is the new person's name? "
+    name = gets.chomp
+    puts "What is the person's phone number? "
+    phone_number = gets.chomp
+    puts "What is the person's address? "
+    address = gets.chomp
+    puts "What is the person's position or title? "
+    position = gets.chomp
+    puts "What is the person's salary? "
+    salary = gets.chomp
+    puts "What is the person's slack handle? "
+    slack_account = gets.chomp
+    puts "What is the person's GitHub user? "
+    github_account = gets.chomp
 
-      new_person = Person.new(name, phone_number, address, position, salary, slack_account, github_account)
+    new_person = Person.new(name, phone_number, address, position, salary, slack_account, github_account)
 
-      @people << new_person
+    @people << new_person
 
+    csv_edit
+    end
   end
 
   def search_peep
@@ -65,7 +59,7 @@ class Database
       puts "The employee's GitHub user is #{found_person.github_account}"
     else
       puts "The person does not exist in TIY"
-    end
+  end
   end
 
   def delete_peep
@@ -79,11 +73,20 @@ class Database
       finder = true
       @people.slice!(index)
       puts "#{delete} has been deleted."
+      csv_edit
     end
     index += 1
     if finder == false
       puts "The person is not in TIY"
       puts "thank you for chosing TIY, good bye!"
+    end
+  end
+
+def csv_edit
+  CSV.open("employees.csv", "w") do |csv|
+    csv << ["name, phone_number", "address", "position", "salary", "slack_account", "github_account"]
+    @people.each do |new_person|
+    csv << [new_person.name, new_person.phone_number, new_person.address, new_person.position, new_person.salary, new_person.slack_account, new_person.github_account]
     end
   end
 end
@@ -97,3 +100,7 @@ loop do
   database.delete_peep if selection == "d"
   database.search_peep if selection == "s"
 end
+# name,phone,address,position,salary,slack,github
+# Gavin,555-1212,1 Main Street,Instructor,1000000,gstark,gstark
+# Jason,555-4242,500 Elm Street,Instructor,2000000,ambethia,ambethia
+# Toni,555-4444,200 Pine Street,Campus Director,3000000,amazing_toni,amazing_toni
